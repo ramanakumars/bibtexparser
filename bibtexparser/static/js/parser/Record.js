bdff31fe553ccc3f8e70a676103362e1af80d503
Ramanakumar Sankar <ramanakumar.shankar@gmail.com>
2025-03-10 23:08:51 -0700

    fixing parsing errors

--- bibtexparser/static/js/parser/Record.js
+++ bibtexparser/static/js/parser/Record.js
@@ -17,15 +17,16 @@ export default class Record {
     entry_name is the ID of the entry
     */
 
-    constructor(rec_type, entry_name) {
+    constructor(rec_type, entry_name, full_text) {
         this.authors = [];
         this.rec_type = rec_type;
         this.entry_name = entry_name;
+        this.full_text = full_text;
     }
 
     parse_text(input_text) {
         this.text = input_text
-        const text  = input_text.replace("[\s]{2,50}?|[\n?]", "");
+        const text = input_text.replace("[\s]{2,50}?|[\n?]", "");
 
         // the entry pattern is [key] = [entry],
         // text_pattern = r"(\w+)\s*=\s*\"?\{*\"?(.*?)\"?\}*\"?,?(,|$)"
@@ -37,53 +38,54 @@ export default class Record {
             let key = match[3].toLowerCase().trim();
             let value = match[4].trim().replace('"', "");
 
-            switch(key) {
-                case "author":
-                case "authors":
-                    const authors = value.split(" and");
+            if ((key == 'author') || (key == 'authors')) {
+                const authors = value.split(" and");
 
-                    // get the list of authors
-                    for (const author of authors) {
-                        let authi = new Author(author.trim());
-                        this.authors.push(authi);
-                    }
-                    break;
-                case "journal":
-                    this.journal = value.replace("{", "").replace("}", "");
+                // get the list of authors
+                for (const author of authors) {
+                    let authi = new Author(author.trim());
+                    this.authors.push(authi);
+                }
+            } else {
+                value = value.replace(/^\{/, "").replace(/\}$/, "")
+                switch (key) {
+                    case "journal":
+                        this.journal = value;
 
-                    // check if the journal is a macro
-                    if(this.journal[0] === "\\") {
-                        if (journal_macros[this.journal]) {
-                            this.journal = journal_macros[this.journal];
-                        } else {
-                            console.log(`Error: Journal macro ${this.journal} not found for entry ${this.entry_name}`);
+                        // check if the journal is a macro
+                        if (this.journal[0] === "\\") {
+                            if (journal_macros[this.journal]) {
+                                this.journal = journal_macros[this.journal];
+                            } else {
+                                console.log(`Error: Journal macro ${this.journal} not found for entry ${this.entry_name}`);
+                            }
                         }
-                    }
-                    break;
-                case "year":
+                        break;
+                    case "year":
 
-                    // convert hte year to an integer
-                    this.year = Number(value.replace("{", "").replace("}", ""));
-                    break;
-                case "month":
-                    // check if the month is an integer or a month name
-                    value = value.replace("{", "").replace("}", "");
-                    if(checkInt(value)) {
-                        this.month = Number(value);
-                    } else {
-                        this.month = value;
-                    }
-                    break;
-                case "volume":
-                    this.volume = Number(value.replace("{", "").replace("}", ""));
-                    break;
-                case "doi":
-                    this.doi = value;
-                    this.doiurl = `https://doi.org/${value}`;
-                    break;
-                default:
-                    this[key] = value.replace(/[{}]/, "");
-                    break;
+                        // convert hte year to an integer
+                        this.year = Number(value);
+                        break;
+                    case "month":
+                        // check if the month is an integer or a month name
+                        if (checkInt(value)) {
+                            this.month = Number(value);
+                        } else {
+                            this.month = value;
+                        }
+                        break;
+                    case "volume":
+                        this.volume = Number(value.replace("{", "").replace("}", ""));
+                        break;
+                    case "doi":
+                        this.doi = value;
+                        this.doiurl = `https://doi.org/${value}`;
+                        break;
+                    default:
+                        this[key] = value;
+                        break;
+                }
+
             }
         }
     }
