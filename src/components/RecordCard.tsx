3243277e68da6652e770e3172c3516cb6db846de
Ramanakumar Sankar <ramanakumar.shankar@gmail.com>
2025-04-07 22:03:23 -0700

    adding more entries to recordcard

--- src/components/RecordCard.tsx
+++ src/components/RecordCard.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import Record from '../parser/Record';
-import { FaChevronDown, FaChevronLeft } from "react-icons/fa";
+import { FaChevronDown, FaChevronUp } from "react-icons/fa";
 
 interface RecordCardProps {
     record: Record;
@@ -13,16 +13,21 @@ const RecordCard: React.FC<RecordCardProps> = ({ record }) => {
         <section className="record">
             <span className="record-header-container">
                 <div className="record-header">
-                    <h2 className="title">{record.title}   ({record.year})</h2>
-                    <h3 className="subtitle">{record.rec_type.toUpperCase()} ({record.entry_name})</h3>
+                    <h2 className="title">{record.title}</h2>
+                    <h3 className="subtitle">Type: {record.rec_type.toUpperCase()}</h3>
+                    <h3 className="subtitle">Key: {record.entry_name}</h3>
+                    <h3 className="subtitle">Year: {record.year}</h3>
                     <div className="author-container">
+                        <span className='author'>
+                            Authors: 
+                        </span>
                         {record.authors.slice(0, 5).map((author, index) => (
                             <span key={index} className="author">{author.short_name}</span>
                         ))}
                     </div>
                 </div>
                 <button onClick={() => setShowText(!showText)}>
-                    {!showText ? <FaChevronDown /> : <FaChevronLeft />}
+                    {!showText ? <FaChevronDown /> : <FaChevronUp />}
                 </button>
             </span>
             { showText && 
